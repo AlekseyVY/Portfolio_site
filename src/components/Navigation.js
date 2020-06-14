37ce46a3b808cbc700b5061ca0f112ea27a08506
AlekseyVY <alekseyVY@gmail.com>
2020-06-14 22:02:03 +0400

    added conditional rendering for mobile view

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,6 @@ import {  BrowserRouter as Router,
     NavLink} from 'react-router-dom'
 import ProjectView from "./views/ProjectsView";
 import EducationView from "./views/EducationView";
-import BlogView from "./views/BlogView";
 import ContactsView from "./views/ContactsView";
 import {CSSTransition} from "react-transition-group";
 /*
@@ -21,43 +20,60 @@ const Navigation = () => {
     useEffect(() => {
         setState(true)
     },[state])
-    return (
-        <Router>
-            <div  className={'navContainer'}>
-                <div  className={'navBar'}>
-                    <NavLink onClick={() => setState(false)} to={'/home'} className={'navBtn'} activeClassName={'selected'}>Home</NavLink>
-                    <NavLink onClick={() => setState(false)} to={'/projects'} className={'navBtn'} activeClassName={'selected'}>Projects</NavLink>
-                    <NavLink onClick={() => setState(false)} to={'/education'} className={'navBtn'} activeClassName={'selected'}>Education</NavLink>
-                    <NavLink onClick={() => setState(false)} to={'contacts'} className={'navBtn'} activeClassName={'selected'}>Contacts</NavLink>
-                </div>
-                <div className={'divMainContent'}>
-                    <Switch>
-                        <Route path={'/projects'}>
-                            <CSSTransition in={state} timeout={200} classNames="switchAnimation">
-                            <ProjectView />
-                            </CSSTransition>
-                        </Route>
-                        <Route path={'/education'}>
-                            <CSSTransition in={state} timeout={200} classNames="switchAnimation">
-                            <EducationView />
-                            </CSSTransition>
-                            </Route>
-                        <Route path={'/contacts'}>
-                            <CSSTransition in={state} timeout={200} classNames="switchAnimation">
-                            <ContactsView />
-                            </CSSTransition>
-                            </Route>
-                        <Route path={'/home'}>
-                            <CSSTransition in={state} timeout={200} classNames="switchAnimation">
-                            <HomeView />
-                            </CSSTransition>
-                            </Route>
-                        <Route path={'/'}><HomeView /></Route>
-                    </Switch>
-                </div>
+
+    let is_mobile = window.matchMedia("only screen and (max-width: 760px)").matches
+
+    if (is_mobile) {
+        return (
+            <div>
+                <h1>
+                    404 (MOBILE VIEW IS IN DEVELOPMENT)
+                </h1>
             </div>
-        </Router>
-    )
+        )
+    } else {
+        return (
+            <Router>
+                <div className={'navContainer'}>
+                    <div className={'navBar'}>
+                        <NavLink onClick={() => setState(false)} to={'/home'} className={'navBtn'}
+                                 activeClassName={'selected'}>Home</NavLink>
+                        <NavLink onClick={() => setState(false)} to={'/projects'} className={'navBtn'}
+                                 activeClassName={'selected'}>Projects</NavLink>
+                        <NavLink onClick={() => setState(false)} to={'/education'} className={'navBtn'}
+                                 activeClassName={'selected'}>Education</NavLink>
+                        <NavLink onClick={() => setState(false)} to={'contacts'} className={'navBtn'}
+                                 activeClassName={'selected'}>Contacts</NavLink>
+                    </div>
+                    <div className={'divMainContent'}>
+                        <Switch>
+                            <Route path={'/projects'}>
+                                <CSSTransition in={state} timeout={200} classNames="switchAnimation">
+                                    <ProjectView/>
+                                </CSSTransition>
+                            </Route>
+                            <Route path={'/education'}>
+                                <CSSTransition in={state} timeout={200} classNames="switchAnimation">
+                                    <EducationView/>
+                                </CSSTransition>
+                            </Route>
+                            <Route path={'/contacts'}>
+                                <CSSTransition in={state} timeout={200} classNames="switchAnimation">
+                                    <ContactsView/>
+                                </CSSTransition>
+                            </Route>
+                            <Route path={'/home'}>
+                                <CSSTransition in={state} timeout={200} classNames="switchAnimation">
+                                    <HomeView/>
+                                </CSSTransition>
+                            </Route>
+                            <Route path={'/'}><HomeView/></Route>
+                        </Switch>
+                    </div>
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default Navigation
