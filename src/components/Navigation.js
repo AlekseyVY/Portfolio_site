52c2dbf30e3f9ed0d37997071a765946722c78cf
AlekseyVY <alekseyVY@gmail.com>
2020-06-14 18:53:51 +0400

    added transition animation when switching components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import HomeView from "./views/HomeView";
-import {BrowserRouter as Router, Switch, Route, NavLink} from 'react-router-dom'
+import {  BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink} from 'react-router-dom'
 import ProjectView from "./views/ProjectsView";
 import EducationView from "./views/EducationView";
 import BlogView from "./views/BlogView";
 import ContactsView from "./views/ContactsView";
+import {CSSTransition} from "react-transition-group";
 /*
 Main navigation component left-side
 with routing
@@ -12,24 +16,42 @@ with routing
 
 
 const Navigation = () => {
+    const [state, setState] = useState(false)
 
-
+    useEffect(() => {
+        setState(true)
+    },[state])
     return (
         <Router>
             <div  className={'navContainer'}>
                 <div  className={'navBar'}>
-                    <NavLink to={'/home'} className={'navBtn'} activeClassName={'selected'}>Home</NavLink>
-                    <NavLink to={'/projects'} className={'navBtn'} activeClassName={'selected'}>Projects</NavLink>
-                    <NavLink to={'/education'} className={'navBtn'} activeClassName={'selected'}>Education</NavLink>
-                    <NavLink to={'contacts'} className={'navBtn'} activeClassName={'selected'}>Contacts</NavLink>
+                    <NavLink onClick={() => setState(false)} to={'/home'} className={'navBtn'} activeClassName={'selected'}>Home</NavLink>
+                    <NavLink onClick={() => setState(false)} to={'/projects'} className={'navBtn'} activeClassName={'selected'}>Projects</NavLink>
+                    <NavLink onClick={() => setState(false)} to={'/education'} className={'navBtn'} activeClassName={'selected'}>Education</NavLink>
+                    <NavLink onClick={() => setState(false)} to={'contacts'} className={'navBtn'} activeClassName={'selected'}>Contacts</NavLink>
                 </div>
                 <div className={'divMainContent'}>
                     <Switch>
-                        <Route path={'/projects'}><ProjectView /></Route>
-                        <Route path={'/education'}><EducationView /></Route>
-                        <Route path={'/blog'}><BlogView /></Route>
-                        <Route path={'/contacts'}><ContactsView /></Route>
-                        <Route path={'/home'}><HomeView /></Route>
+                        <Route path={'/projects'}>
+                            <CSSTransition in={state} timeout={200} classNames="switchAnimation">
+                            <ProjectView />
+                            </CSSTransition>
+                        </Route>
+                        <Route path={'/education'}>
+                            <CSSTransition in={state} timeout={200} classNames="switchAnimation">
+                            <EducationView />
+                            </CSSTransition>
+                            </Route>
+                        <Route path={'/contacts'}>
+                            <CSSTransition in={state} timeout={200} classNames="switchAnimation">
+                            <ContactsView />
+                            </CSSTransition>
+                            </Route>
+                        <Route path={'/home'}>
+                            <CSSTransition in={state} timeout={200} classNames="switchAnimation">
+                            <HomeView />
+                            </CSSTransition>
+                            </Route>
                         <Route path={'/'}><HomeView /></Route>
                     </Switch>
                 </div>
