3f9651b6c51c0230e50db85cae9c5a17bb312b36
AlekseyVY <alekseyvy@gmail.com>
2020-08-09 12:16:33 +0000

    fix perfomance issue

--- js/main.js
+++ js/main.js
@@ -39,19 +39,6 @@
     }, {offset: '80%'});
     
     
-    // Porfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('filter-active');
-        $(this).addClass('filter-active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
-    
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
